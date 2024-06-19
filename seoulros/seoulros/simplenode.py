c6380598d4bf2ee3e75169291dcda89d56900975
freshmea <freshmea@gmail.com>
2024-06-18 19:44:16 -0700

    aa

--- seoulros/seoulros/simplenode.py
+++ seoulros/seoulros/simplenode.py
@@ -1,5 +1,18 @@
+from rclpy import Node
+import rclpy
+
+i = 0
+
+def printtest():
+    print('ros test', i)
+    i += 1
+
 def main():
-    print('ros test')
+    rclpy.init()
+    node = Node()
+    node.create_timer(1, printtest)
+    rclpy.spin(node)
+
 
 if __name__ == '__main__':
     main()
